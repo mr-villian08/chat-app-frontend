d8e0bb3f6f150178cf178000dcb7b808dcbf8a08
Akshay Sharma <akshay.sharma2@idsil.com>
2024-07-04 17:44:14 +0530

    working on it

--- src/components/ChatItem/SendMessage.jsx
+++ src/components/ChatItem/SendMessage.jsx
@@ -21,7 +21,7 @@ const SendMessage = ({
             type="text"
             placeholder="Enter Message..."
             value={message}
-            className="pl-4 p-2 2xl:w-[1220px] xl:w-[730px] bg-gray-800 rounded-lg outline-none"
+            className="pl-4 p-2 2xl:w-[1220px] xl:w-[730px] md:w-[560px] bg-gray-800 rounded-lg outline-none"
             onChange={onChangeMessageHandler}
           />
           <div className="flex relative items-center text-center gap-6">
@@ -54,7 +54,7 @@ const SendMessage = ({
               htmlFor="attachment-input"
               className="relative group cursor-pointer hover:translate-y-1 hover:rounded-[50%] transition-all duration-500 hover:from-[#331029] hover:to-[#310413] hover:bg-gradient-to-r from-gray-800 to-black"
             >
-              <img src={attachmentIcon} alt="Attachment" className="h-6 w-6" />
+              <img src={attachmentIcon} alt="Attachment" className="h-5 w-5" />
               <span className="absolute opacity-0 group-hover:opacity-100 group-hover:dark:text-white group-hover:text-sm group-hover:text-black group-hover:-translate-y-14 -left-7 duration-700">
                 Attachment
               </span>
@@ -85,7 +85,7 @@ const SendMessage = ({
                 // onChange={(e) => handleImageUpload(e.target.files)}
               />
             </label>
-            <button className="flex items-center bg-green-700 gap-1 px-3 py-1.5 cursor-pointer font-semibold tracking-widest rounded-md hover:bg-green-600 duration-300 hover:gap-2 hover:translate-x-3">
+            <button className="flex items-center bg-teal-500 gap-1 px-3 py-1.5 cursor-pointer font-semibold tracking-widest rounded-md hover:bg-teal-600 duration-300 hover:gap-2 hover:translate-x-3">
               <img src={sendIcon} alt="Send" />
             </button>
           </div>
